92e0dc219afba8fe82c8b694d32b3b91a0bdf3ef
Alex Gabriel <alex.gabriel@live.ca>
2023-09-07 15:56:28 -0400

    Updated to full deployment

--- main.tf
+++ main.tf
@@ -12,82 +12,114 @@ terraform {
 provider "aws" {
   region = "ca-central-1"
 }
-/*
-#vpc creation
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
 
-  name = "Terraform and Jenkins VPC"
-  cidr = "172.16.0.0/16"
-
-  azs             = ["ca-central-1a"]
-  private_subnets = ["172.16.1.0/24"]
-  public_subnets  = ["172.16.101.0/24"]
-
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+resource "aws_vpc" "some_custom_vpc" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "Terraform VPC"
   }
 }
-*/
-module "web_server_sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
-  name        = "Terraform and Jenkins Web Server"
-  description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = module.vpc.vpc_id
+resource "aws_subnet" "some_public_subnet" {
+  vpc_id            = aws_vpc.some_custom_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ca-central-1a"
 
-  ingress_cidr_blocks = ["172.16.1.0/24"]
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "Terraform Public Subnet"
   }
 }
-/*
-data "aws_ami" "ubuntu" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+resource "aws_subnet" "some_private_subnet" {
+  vpc_id            = aws_vpc.some_custom_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ca-central-1a"
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-*/
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = web_server_sg
   tags = {
-    Name = "Terraform and Jenkins Web Server"
+    Name = "Terraform Private Subnet"
   }
 }
-/*
-resource "aws_route53_zone" "example" {
-  name = "bell.ca"
+
+resource "aws_internet_gateway" "some_ig" {
+  vpc_id = aws_vpc.some_custom_vpc.id
+
+  tags = {
+    Name = "Terraform Internet Gateway"
+  }
 }
 
-resource "aws_route53_record" "example" {
-  allow_overwrite = true
-  name            = "bell.ca"
-  ttl             = 172800
-  type            = "NS"
-  zone_id         = aws_route53_zone.example.zone_id
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.some_custom_vpc.id
 
-  records = [
-    aws_route53_zone.example.name_servers[0],
-    aws_route53_zone.example.name_servers[1],
-    aws_route53_zone.example.name_servers[2],
-    aws_route53_zone.example.name_servers[3],
-  ]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.some_ig.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.some_ig.id
+  }
+
+  tags = {
+    Name = "Terraform Route Table"
+  }
 }
-*/
+
+resource "aws_route_table_association" "public_1_rt_a" {
+  subnet_id      = aws_subnet.some_public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "web_sg" {
+  name   = "Terraform HTTP and SSH"
+  vpc_id = aws_vpc.some_custom_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_instance" {
+  ami           = "ami-0b4dc0f8083ae6a57"
+  instance_type = "t2.micro"
+  #key_name      = "MyKeyPair"
+
+  subnet_id                   = aws_subnet.some_public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+  #!/bin/bash -ex
+
+  sudo yum install -y nginx1
+  sudo systemctl enable nginx
+  sudo systemctl start nginx
+  EOF
+
+  tags = {
+    "Name" : "Kanye"
+  }
+}
+
+output "ec2_global_ips" {
+  value = ["${aws_instance.web_instance.*.public_ip}"]
+}
