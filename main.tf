32f4c38f618e734f866ec0bd016823d4ebafc488
Alex Gabriel <alex.gabriel@live.ca>
2023-09-11 11:45:44 -0400

    Switched Apache to NGINX

--- main.tf
+++ main.tf
@@ -111,10 +111,9 @@ resource "aws_instance" "web_instance" {
 # Use this for your user data (script from top to bottom)
 # install httpd (Linux 2 version)
 yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+yum install -y nginx
+systemctl start nginx
+systemctl enable nginx
   EOF
 
   tags = {
