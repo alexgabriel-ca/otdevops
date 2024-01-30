b22dd38e77ad9aec0989a9e4e9cac29b9b50c082
Alex Gabriel <alex@alexgabriel.ca>
2024-01-30 10:18:51 -0500

    Removed availability zone specification

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "devops_public_subnet" {
   vpc_id            = aws_vpc.devops_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ca-central-1a"
+  #availability_zone = "ca-central-1a"
 
   tags = {
     Name        = "OpenText DevOps Public Subnet"
@@ -12,7 +12,7 @@ resource "aws_subnet" "devops_public_subnet" {
 resource "aws_subnet" "devops_private_subnet" {
   vpc_id            = aws_vpc.devops_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "ca-central-1a"
+  #availability_zone = "ca-central-1a"
 
   tags = {
     Name        = "OpenText DevOps Private Subnet"
